d4c61c0756db51c30a161ad82d9e7902a2cf9f62
Amin <aminnazari55@gmail.com>
2018-04-15 15:43:59 +0430

    Update site key

--- src/js/contact/components/form.vue
+++ src/js/contact/components/form.vue
@@ -56,7 +56,7 @@ export default {
             email: null,
             name: null,
             phone: null,
-            siteKey: '6Lcrtc4SAAAAAKekvip0yI9Dp2DKXyVtliBXnqHS',
+            siteKey: '6LcDpDcUAAAAAMiXOz1gA3By9oEJ4-PYqct1Ihn5',
             g_recaptcha: '',
 
 			/**
