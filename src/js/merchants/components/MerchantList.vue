23e505d9cb354a65df98a931c1b15d7a7c785f01
Amin <aminnazari55@gmail.com>
2018-04-23 13:07:09 +0430

    Change merchants route

--- src/js/merchants/components/MerchantList.vue
+++ src/js/merchants/components/MerchantList.vue
@@ -46,7 +46,7 @@ export default {
 		fetchData(cat, pageNum) {
 
 		    let vm = this;
-            axios.get(`https://api.zarinpal.com/panel/frontPage/webServicesList/${cat}.json?page=${pageNum}`)
+            axios.get(`https://api.zarinpal.com/rest/v3/merchantStatistics/webServicesList/${cat}.json?page=${pageNum}`)
                 .then(function (response) {
                     vm.catInfo = response.data.catInfo;
                     vm.merchantItems = response.data.list;
